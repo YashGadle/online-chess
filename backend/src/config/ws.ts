56ada7ece859358ccf4d0a822e4ae420d63f9d1b
Yash Gadle <yashgadle@gmail.com>
2026-01-04 00:02:45 +0530

    refactored web socket logic to work with prod

--- backend/src/config/ws.ts
+++ backend/src/config/ws.ts
@@ -12,56 +12,46 @@ import { signalStartGame } from "../routes/chess";
 import handleChessGame from "../routes/chess";
 
 export function setupWebSocket(server: HttpServer) {
-  const wss = new WebSocketServer({ noServer: true });
-
-  server.on("upgrade", (request, socket, head) => {
-    if (!request.url?.startsWith("/ws")) {
-      socket.destroy();
+  const wss = new WebSocketServer({ server });
+  const clients = new Map<string, WebSocket>();
+  wss.on("connection", async (ws, request: any) => {
+    if (!request.url?.startsWith("/ws/game/")) {
+      ws.close(1008, "Invalid path");
       return;
     }
-
-    sessionParser(request as any, {} as any, () => {
-      //@ts-ignore: Property 'session' does not exist on type 'IncomingMessage'.
+    sessionParser(request, {} as any, async () => {
       if (!request.session?.userId) {
-        socket.write("HTTP/1.1 401 Unauthorized\r\n\r\n");
-        return socket.destroy();
+        ws.close(1008, "Unauthorized");
+        return;
       }
 
-      wss.handleUpgrade(request, socket, head, (ws) => {
-        wss.emit("connection", ws, request);
-      });
-    });
-  });
+      const userId = request.session.userId;
+      //@ts-ignore: Property 'dispatchEvent' is missing in type @types/ws/index
+      clients.set(userId, ws);
 
-  const clients = new Map<string, WebSocket>();
+      const gameId = extractGameIdFromUrl(request.url);
+      const game = await redis.get<GameCache>(gameId);
+      if (game && game.users.length === 2) {
+        signalStartGame(clients, game);
+      }
 
-  wss.on("connection", async (ws, request: any) => {
-    const userId = request.session.userId;
-    //@ts-ignore: Property 'dispatchEvent' is missing in type @types/ws/index
-    clients.set(userId, ws);
+      ws.on("message", async (data: Buffer) => {
+        try {
+          const dataJSON: WSMessageT = JSON.parse(data.toString());
 
-    const gameId = extractGameIdFromUrl(request.url);
-    const game = await redis.get<GameCache>(gameId);
-    if (game && game.users.length === 2) {
-      signalStartGame(clients, game);
-    }
-
-    ws.on("message", async (data: Buffer) => {
-      try {
-        const dataJSON: WSMessageT = JSON.parse(data.toString());
-
-        // Logic to handle chess moves
-        if (dataJSON.type === "move") {
-          //@ts-ignore: Property 'dispatchEvent' is missing in type @types/ws/index
-          handleChessGame(ws, clients, dataJSON, request);
+          // Logic to handle chess moves
+          if (dataJSON.type === "move") {
+            //@ts-ignore: Property 'dispatchEvent' is missing in type @types/ws/index
+            handleChessGame(ws, clients, dataJSON, request);
+          }
+        } catch (error) {
+          console.log("Unable to parse message", error);
         }
-      } catch (error) {
-        console.log("Unable to parse message", error);
-      }
-    });
+      });
 
-    ws.on("close", () => {
-      clients.delete(userId);
+      ws.on("close", () => {
+        clients.delete(userId);
+      });
     });
   });
 
